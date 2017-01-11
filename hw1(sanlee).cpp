df45e7dd3e5d00e6764d161a218431630730ec44
San Lee <sanlee2017@u.northwestern.edu>
2017-01-10 23:39:23 -0600

    hw1

---
+++ hw1(sanlee).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+        cout<<"enter 6 digits of the equation: ";
+        int a;
+        int b;
+        int c;
+        int d;
+        int e;
+        int f;
+
+        cin>>a>>b>>c>>d>>e>>f;
+
+        cout<<"x == "<< (c*e -f*b)/(a*e - d*b);
+
+
+}
