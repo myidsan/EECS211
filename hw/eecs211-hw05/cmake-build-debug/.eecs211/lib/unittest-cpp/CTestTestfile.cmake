3067cb2b417cdf2cc95272a15812b54b4f25f628
San Lee <sanlee2017@u.northwestern.edu>
2017-02-15 22:48:16 -0600

    removing orignal hw5

--- hw/eecs211-hw05/cmake-build-debug/.eecs211/lib/unittest-cpp/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/SanLee/Desktop/EECS211/hw/eecs211-hw05/.eecs211/lib/unittest-cpp
-# Build directory: /Users/SanLee/Desktop/EECS211/hw/eecs211-hw05/cmake-build-debug/.eecs211/lib/unittest-cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- hw/eecs211-hw05/parsing.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "interface.h"
-
-#include <string>
-#include <vector>
-
-int parse_int(const std::string& s);
-std::vector<std::string> tokenize(const std::string& line);
